6c7b557e48ae86108f240f131b0d3e98e89c8764
Patel-Krish26 <krishpr2004@gmail.com>
2026-05-22 18:18:23 +0530

    Refactor Swagger setup and add health endpoint
    Updated Swagger configuration and health endpoint.

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using backEnd.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
@@ -15,38 +14,26 @@ builder.Services.AddAngularCors();
 
 var app = builder.Build();
 
-// Global Exception Middleware
 app.UseGlobalExceptionMiddleware();
 
-// Swagger ONLY in Development
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FarmEase API v1");
-        c.RoutePrefix = string.Empty;
-    });
-}
-
-// IMPORTANT FOR RENDER
-// DO NOT FORCE HTTPS
-// app.UseHttpsRedirection();
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "FarmEase API v1");
+});
 
 app.UseRouting();
 
-// CORS
 app.UseCors("AllowAngularApp");
 
-// Authentication
 app.UseAuthentication();
 app.UseAuthorization();
 
-// Controllers
 app.MapControllers();
 
-// Health endpoint for Render
 app.MapGet("/", () => "FarmEase API Running");
+
+app.MapGet("/health", () => "Healthy");
 
 app.Run();
